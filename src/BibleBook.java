7c33852b8f0f61795538cc6fa52e82fa37da8f5b
emily-sear <61364884+emily-sear@users.noreply.github.com>
2020-09-21 10:06:06 -0500

    initial push

---
+++ src/BibleBook.java
@@ -0,0 +1,13 @@
+public class BibleBook 
+{
+	private String bookName;
+	private int numOfChapters;
+	private String summaryOfBook;
+	
+	public BibleBook(String bookName, int numofChapters, String summaryOfBook)
+	{
+		this.bookName = bookName;
+		this.numOfChapters = numofChapters;
+		this.summaryOfBook = summaryOfBook;
+	}
+}
